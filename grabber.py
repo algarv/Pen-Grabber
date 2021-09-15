9290daf729a1640fa2af34491edf0e1b03f2edf5
Anna Garverick <annagarverick2022@u.northwestern.edu>
2021-09-15 11:57:07 -0500

    Open and display and image

--- grabber.py
+++ grabber.py
@@ -1,19 +1,33 @@
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
 import pyrealsense2 as rs
 import numpy as np
-import cv2
-
+import cv2 as cv
+import matplotlib
+from matplotlib import pyplot as plt
 robot = InterbotixManipulatorXS("px100", "arm", "gripper")
+
+purple = [130,0,75]
+
+
+## Camera set-up ##
+
+
+##Image set-up and display##
+
+img = cv.imread('Northwestern.png')
+cv.imshow("Logo",img)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+##Measure the pen location##
+
+
 
 ##Move Arm to its starting position##
 
 
 
 ##Open the grippers##
-
-
-
-##Measure the pen location##
 
 
 
